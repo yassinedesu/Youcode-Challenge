d5b88ee6f6409669dded33c3623b3efde2b70d8f
Yassine El khader <yassydesuwork@gmail.com>
2024-09-24 20:52:50 +0100

    C Files

---
+++ challenge3.c
@@ -0,0 +1,12 @@
+/* Challenge 3*/
+#include <stdio.h>
+
+int main(){
+    float Km,Yards;
+    
+    printf("Veuillez entrer le nombre de Km a convertir : ");
+    scanf("%f",&Km);
+    Yards = (Km * 1093.61);
+    printf(" %f Km = %.2f Yards",Km,Yards);
+
+}
