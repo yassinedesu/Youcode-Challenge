3f60dd19b214c0ee1305164f8469f4ff5e52b56e
unknown <yassydesuwork@gmail.com>
2024-09-25 11:15:05 +0200

    Done

--- challenge12.c
+++ challenge12.c
@@ -1,2 +1,15 @@
 // Challenge 12
 
+#include <stdio.h>
+
+int main (){
+    int n, unite, dizaine, centaine, millier;
+    printf("Veuillez entrer un nombre entier de 4 chiffre a inverse : ");
+    scanf("%d",&n);
+    millier = n / 1000;
+    centaine = (n / 100) % 10 ;
+    dizaine = (n / 10) % 10 ;
+    unite = (n % 10) ;
+    printf("%d%d%d%d",unite,dizaine,centaine,millier);
+    return 0;
+}
